de2779aef4c6cd161a92598a33d28263a5b0d081
BlackAngel <sefsef12364@gmail.com>
2019-12-26 11:30:42 +0000

    +ziko

--- src/main/java/com/projectHbase/javaHbase/controller/ReponseController.java
+++ src/main/java/com/projectHbase/javaHbase/controller/ReponseController.java
@@ -1,5 +1,21 @@
 package com.projectHbase.javaHbase.controller;
 
+import com.projectHbase.javaHbase.Reponse;
+import com.projectHbase.javaHbase.ReponseRepository;
+import com.projectHbase.javaHbase.User;
+import java.io.IOException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ReponseController {
+    
+    ReponseRepository repository=new ReponseRepository();
+  @PostMapping("/")
+    public void addUser(@RequestBody Reponse reponse) throws IOException {
+        repository.save(reponse);
+
+    }
 
 }
