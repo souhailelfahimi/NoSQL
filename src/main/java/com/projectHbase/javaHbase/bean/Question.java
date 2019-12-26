78c148f68ef66a2c9d52576662da6b87023a9ea1
ayouball <32517215+ayouball@users.noreply.github.com>
2019-12-26 14:08:59 +0100

    QuestionBean
    QuestionBean

--- src/main/java/com/projectHbase/javaHbase/bean/Question.java
+++ src/main/java/com/projectHbase/javaHbase/bean/Question.java
@@ -2,6 +2,7 @@ package com.projectHbase.javaHbase.bean;
 
 public class Question {
 	 private String id;
+	 private String _id_user;
 	 private String titre;
 	 private String description;
 	 
@@ -22,5 +23,11 @@ public class Question {
 	}
 	 public void setTitre(String titre) {
 		this.titre = titre;
+	}
+	public String get_id_user() {
+		return _id_user;
+	}
+	 public void set_id_user(String _id_user) {
+		this._id_user = _id_user;
 	}
 }
